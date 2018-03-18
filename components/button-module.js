957ce13e8c6687ab89701bbb275f24365fac7a92
Andrew <twintrool@gmail.com>
2018-03-17 23:21:21 -0500

    init

---
+++ components/button-module.js
@@ -0,0 +1,15 @@
+import React from "react";
+import Button from "material-ui/Button";
+
+class ButtonModule extends React.Component {
+  render() {
+    return (
+      <div>
+        Here is the Button Module Component
+        <Button>button</Button>
+      </div>
+    );
+  }
+}
+
+export default ButtonModule;
---
+++ pages/index.js
@@ -0,0 +1,15 @@
+import React from "react";
+import ButtonModule from "../components/button-module";
+
+class Index extends React.Component {
+  render() {
+    return (
+      <div>
+        <div>Welcome to React</div>
+        <ButtonModule />
+      </div>
+    );
+  }
+}
+
+export default Index;
